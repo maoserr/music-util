e427af93edbfc454c503e46fd2d67887723cc8d6
maoserr <maoserr@gmail.com>
2023-11-27 11:09:42 -0600

    update

--- music_util/main.py
+++ music_util/main.py
@@ -1,7 +1,6 @@
 import sys
-import tkinter
-from tkinter import ttk, Frame, PhotoImage
-from tkinter import filedialog as fd
+import tkinter as tk
+from tkinter import ttk, filedialog
 from multiprocessing import Process
 
 
@@ -11,39 +10,49 @@ def demucs_exec(args: list):
     separate.main(args)
 
 
-class MainApplication(Frame):
-    filename = ""
-
+class VocalRemover(tk.Frame):
     def __init__(self, root, *args, **kwargs):
-        Frame.__init__(self, root, *args, **kwargs)
+        tk.Frame.__init__(self, root, *args, **kwargs)
         self.grid()
 
+        self.filename = tk.StringVar(root, "")
+        self.outfile = tk.StringVar(root, "")
+
         ttk.Label(self, text="MP3 File:").grid(column=0, row=0, padx=10, pady=10)
-        ttk.Entry(self).grid(column=1, row=0, padx=10, pady=10)
-        ttk.Button(self, text="...", command=self.set_file).grid(column=2, row=0)
-        ttk.Button(self, text="Run Demucs", command=self.run_demucs).grid(column=3, row=0)
+        ttk.Entry(self, textvariable=self.filename, width=50).grid(column=1, row=0, padx=10, pady=10)
+        ttk.Button(self, text="...", command=self.set_file).grid(column=2, row=0, padx=10, pady=10)
+        ttk.Button(self, text="Run Remover", command=self.run_demucs).grid(column=2, row=2)
+        ttk.Button(self, text="Set output", command=self.set_outfile).grid(column=0, row=1)
+        ttk.Entry(self, textvariable=self.outfile, width=50).grid(column=1,row=1)
 
     def set_file(self):
-        self.filename = fd.askopenfilename(title='Select MP3',
-                                           initialdir='/',
-                                           filetypes=(("MP3", "*.mp3"),))
+        self.filename.set(tk.filedialog.askopenfilename(title='Select MP3',
+                                                        initialdir='/',
+                                                        filetypes=(("MP3", "*.mp3"),)))
+
+    def set_outfile(self):
+        self.outfile.set(tk.filedialog.asksaveasfilename(title='Select output location',
+                                                         filetypes=(("Folder","*.*"),)))
 
     def run_demucs(self):
         p = Process(target=demucs_exec, args=(["--two-stems", "vocals", "--mp3", "-n", "htdemucs_ft"]
-                                              + [self.filename],))
+                                              + [self.filename.get()],))
         p.start()
 
 
 def main(argv: list):
     import sv_ttk
 
-    root = tkinter.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root = tk.Tk()
+    tab_c = ttk.Notebook(root)
+    voc_remover = VocalRemover(tab_c, pady=10, padx=10)
+    tab_c.add(voc_remover, text="Vocal Remover")
+    tab_c.pack(expand=1, fill="both")
 
     sv_ttk.set_theme("dark")
     root.title("Musician Utilities")
-    root.geometry("600x400")
-    root.iconphoto(False, PhotoImage(file='icon.png'))
+    root.iconphoto(False, tk.PhotoImage(file='icon.png'))
+    root.resizable(False, False)
     root.mainloop()
 
 
