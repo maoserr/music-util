492415da8edfeaa6867a19358258c9a9924daf6b
maoserr <maoserr@gmail.com>
2024-04-14 16:07:21 -0500

    wip

--- src/music_util/transcr.py
+++ src/music_util/transcr.py
@@ -11,9 +11,9 @@ import pandas as pd
 def get_notes(notes: pd.DataFrame, hop_duration=10) -> pd.DataFrame:
     notes['note'] = librosa.hz_to_note(notes['pitch'])
     notes['chg'] = notes.note.ne(notes.note.shift())
-    notes['notes_size'] = notes['notes_chg'].cumsum()
+    notes['note_num'] = notes['chg'].cumsum()
 
-    notes_uniq = notes.groupby('notes_size').agg({
+    notes_uniq = notes.groupby('note_num').agg({
         'pitch': 'mean',
         'confidence': 'mean',
         'note': 'first',
@@ -50,26 +50,58 @@ def smooth_notes(notes: pd.DataFrame, min_length: float) -> pd.DataFrame:
     })
 
     # Remove note name from rests
-    nugr.loc[~nugr['not_rest'],'note']=''
+    nugr.loc[~nugr['not_rest'], 'note'] = ''
 
     # Combine same notes
-    nuc=nugr.groupby(nugr['note'].ne(nugr['note'].shift()).cumsum()).agg({
-        'pitch':'first',
+    nuc = nugr.groupby(nugr['note'].ne(nugr['note'].shift()).cumsum()).agg({
+        'pitch': 'first',
         'confidence': 'mean',
-        'note':'first',
-        'chg':'sum',
-        'not_rest':'first',
-        'duration_ms':'sum'
+        'note': 'first',
+        'chg': 'sum',
+        'not_rest': 'first',
+        'duration_ms': 'sum'
     })
     return nuc
 
 
-def gen_sheet(note_cnts: pd.DataFrame, tempo: float, outfile: str):
-    beat_len_ms = (60 * 1000) / tempo
-    # Sixteenth notes minimum
-    min_note_len = beat_len_ms / 32
-    note_cnts['min_note_cnt'] = (note_cnts.duration_ms / min_note_len).round()
+def gen_sheet(note_cnts: pd.DataFrame,
+              beat_len_ms: float,
+              min_note: int,
+              outfile: str,
+              inbase: str):
+    import numpy as np
+    note_cnts['num_beats'] = note_cnts['duration_ms'] / beat_len_ms
 
+    valid_notes = pd.Series([-np.inf, 1 / 16, 1 / 4, 1 / 2, 1, 2, 4, 8])
+    labels = ['32', '16', '8', '4', '2', '1', '1/2']
+    note_cnts['note_cat'] = pd.cut(note_cnts['num_beats'],
+                                   valid_notes,
+                                   labels=labels).fillna('2W')
+    notations = []
+    for _, r in note_cnts.iterrows():
+        if r["not_rest"]:
+            linote = r["note"][0].lower()
+            octa = r["note"][-1]
+            curr_not = linote
+            if len(r["note"]) == 3:
+                curr_not += 'is'
+            if octa == '4':
+                curr_not += "'"
+            if r["note_cat"] == '1/2':
+                notations.append(curr_not + "1")
+                notations.append(curr_not + "1")
+            else:
+                notations.append(curr_not + r["note_cat"] )
+        else:
+            if r["note_cat"]  == '1/2':
+                notations.append('r1')
+                notations.append('r1')
+            else:
+                notations.append('r'+r["note_cat"] )
+    with open(os.path.join(outfile, f"{inbase}_input.li"),"w") as f:
+        f.write("{\n")
+        f.write(" ".join(notations))
+        f.write("\n}\n")
 
 def crepe_exe(args: list, q: Queue):
     from music_util.utils import StdoutProcRedirect
@@ -83,21 +115,34 @@ def crepe_exe(args: list, q: Queue):
         import os
 
         infiles = args[0]
+        os.makedirs(args[1], exist_ok=True)
         for file in infiles:
+            fbase = os.path.splitext(os.path.basename(file))[0] + "0"
+            while os.path.exists(os.path.join(args[1], f"{fbase}_notes.csv")):
+                fbase = fbase[:-1] + str(int(fbase[-1]) + 1)
             audio_rosa, sr = librosa.load(file, sr=16000, mono=True)
+            print("Getting tempo...")
             tempo, _ = librosa.beat.beat_track(y=audio_rosa, sr=sr)
             if audio_rosa.dtype == np.int16:
                 audio_rosa = audio_rosa.astype(np.float32) / np.iinfo(np.int16).max
 
+            print("Predicting pitch...")
             audio = torch.tensor(np.copy(audio_rosa.transpose()))[None]
             pitch, confidence = torchcrepe.predict(audio, sr, hop_length=160, return_periodicity=True, model="tiny")
             notes = pd.DataFrame({"pitch": pitch.numpy().squeeze(), "confidence": confidence.numpy().squeeze()})
+
+            print("Processing notes...")
             note_cnts = get_notes(notes)
 
             beat_len_ms = (60 * 1000) / tempo
-            min_note_len = beat_len_ms / 32
+            min_note = 32  # 32nd note
+            min_note_len = beat_len_ms / min_note
             note_smooth = smooth_notes(note_cnts, min_note_len)
-            gen_sheet(note_smooth, tempo, args[1])
+
+            note_smooth.to_csv(os.path.join(args[1], f"{fbase}_notes.csv"))
+
+            print("Sheet music...")
+            gen_sheet(note_smooth, beat_len_ms, min_note, args[1], fbase)
         print("Processing complete")
     except:
         import traceback
