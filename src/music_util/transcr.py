a2da966b443805a93cf03f4c78588282edd9005d
Maoserr <maoserr@gmail.com>
2024-04-14 09:04:25 -0500

    wip

--- src/music_util/transcr.py
+++ src/music_util/transcr.py
@@ -8,25 +8,67 @@ import librosa
 import pandas as pd
 
 
-def get_notes(notes: pd.DataFrame, outfile: str, tempo: float, beats: list):
-    beat_len_ms = (60 * 1000) / tempo
-    min_note_len = beat_len_ms / 16
-
+def get_notes(notes: pd.DataFrame, hop_duration=10) -> pd.DataFrame:
+    notes['note'] = librosa.hz_to_note(notes['pitch'])
     notes['chg'] = notes.note.ne(notes.note.shift())
-    notes['not_rest'] = notes.confidence.gt(0.6)
-    notes['notes_chg'] = notes['chg'] & notes['not_rest']
     notes['notes_size'] = notes['notes_chg'].cumsum()
 
     notes_uniq = notes.groupby('notes_size').agg({
-        'pitch': 'first',
-        'confidence': 'first',
+        'pitch': 'mean',
+        'confidence': 'mean',
         'note': 'first',
-        'chg': 'count',
-        'not_rest': 'first',
-        'notes_chg': 'first'
+        'chg': 'count'
     })
-    outdf = notes
-    outdf.to_csv(outfile)
+    notes_uniq['not_rest'] = notes_uniq.confidence.gt(0.6)
+    notes_uniq['duration_ms'] = notes_uniq['chg'] * hop_duration
+    return notes_uniq
+
+
+def smooth_notes(notes: pd.DataFrame, min_length: float) -> pd.DataFrame:
+    notes['rollup'] = False
+    notes.loc[notes['duration_ms'] < min_length, 'rollup'] = True
+
+    # get groups of notes to roll up
+    nusg = notes.groupby(notes['rollup'].ne(notes['rollup'].shift()).cumsum()).agg({
+        'pitch': 'mean',
+        'confidence': 'mean',
+        'note': 'first',
+        'chg': 'sum',
+        'not_rest': 'first',
+        'duration_ms': 'sum',
+        'rollup': 'first'
+    })
+
+    # Roll up to next note
+    nugr = nusg.groupby(nusg['rollup'].eq(True).cumsum()).agg({
+        'pitch': 'last',
+        'confidence': 'last',
+        'note': 'last',
+        'chg': 'sum',
+        'not_rest': 'last',
+        'duration_ms': 'sum',
+    })
+
+    # Remove note name from rests
+    nugr.loc[~nugr['not_rest'],'note']=''
+
+    # Combine same notes
+    nuc=nugr.groupby(nugr['note'].ne(nugr['note'].shift()).cumsum()).agg({
+        'pitch':'first',
+        'confidence': 'mean',
+        'note':'first',
+        'chg':'sum',
+        'not_rest':'first',
+        'duration_ms':'sum'
+    })
+    return nuc
+
+
+def gen_sheet(note_cnts: pd.DataFrame, tempo: float, outfile: str):
+    beat_len_ms = (60 * 1000) / tempo
+    # Sixteenth notes minimum
+    min_note_len = beat_len_ms / 32
+    note_cnts['min_note_cnt'] = (note_cnts.duration_ms / min_note_len).round()
 
 
 def crepe_exe(args: list, q: Queue):
@@ -43,15 +85,19 @@ def crepe_exe(args: list, q: Queue):
         infiles = args[0]
         for file in infiles:
             audio_rosa, sr = librosa.load(file, sr=16000, mono=True)
-            tempo, beats = librosa.beat.beat_track(y=audio_rosa, sr=sr)
+            tempo, _ = librosa.beat.beat_track(y=audio_rosa, sr=sr)
             if audio_rosa.dtype == np.int16:
                 audio_rosa = audio_rosa.astype(np.float32) / np.iinfo(np.int16).max
 
             audio = torch.tensor(np.copy(audio_rosa.transpose()))[None]
-            pitch, confidence = torchcrepe.predict(audio, sr, return_periodicity=True, model="tiny")
+            pitch, confidence = torchcrepe.predict(audio, sr, hop_length=160, return_periodicity=True, model="tiny")
             notes = pd.DataFrame({"pitch": pitch.numpy().squeeze(), "confidence": confidence.numpy().squeeze()})
-            notes['note'] = librosa.hz_to_note(notes['pitch'])
-            get_notes(notes, args[1], tempo, beats)
+            note_cnts = get_notes(notes)
+
+            beat_len_ms = (60 * 1000) / tempo
+            min_note_len = beat_len_ms / 32
+            note_smooth = smooth_notes(note_cnts, min_note_len)
+            gen_sheet(note_smooth, tempo, args[1])
         print("Processing complete")
     except:
         import traceback
